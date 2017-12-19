1ac338868079c85c298c6774d4bd22f35c951cd6
CreaSoEva <33323626+CreaSoEva@users.noreply.github.com>
2017-12-19 16:19:56 +0100

    affichage carte

--- projet_dataapi/app/Http/Controllers/MuseesController.php
+++ projet_dataapi/app/Http/Controllers/MuseesController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Musee;
 use Illuminate\Http\Request;
 
@@ -14,72 +12,9 @@ class MuseesController extends Controller
      */
     public function index()
     {
-        //
-    }
+        
+           $titis = Musee::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Musee  $musee
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Musee $musee)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Musee  $musee
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Musee $musee)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Musee  $musee
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Musee $musee)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Musee  $musee
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Musee $musee)
-    {
-        //
+         return view('sophie' , compact('titis'));           
     }
 }
--- projet_dataapi/app/Musee.php
+++ projet_dataapi/app/Musee.php
@@ -1,10 +1,9 @@
 <?php
-
 namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Musee extends Model
+class Musee 
 {
-    //
+    static function all() {
+    $reponse = file_get_contents('http://www.data.gouv.fr/fr/datasets/r/c08e1921-c538-4092-96ca-538553a4903b');
+    return(json_decode($reponse)->features);
+}
 }
