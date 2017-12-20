facf64deffe52b2c77c6a35735cc1de9b3d0827c
CreaSoEva <33323626+CreaSoEva@users.noreply.github.com>
2017-12-20 16:02:46 +0100

    carte2

---
+++ projet_dataapi/app/Http/Controllers/MuseesController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Musee;
+use Illuminate\Http\Request;
+
+class MuseesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+           $cartes = Musee::all();
+
+           return view('sophie' , compact('cartes'));        
+        
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Musee  $musee
+     * @return \Illuminate\Http\Response
+     */
+    public function show($uid)
+    {
+        $carte = Musee::get($uid);
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Musee  $musee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Musee $musee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Musee  $musee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Musee $musee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Musee  $musee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Musee $musee)
+    {
+        //
+    }
+}
