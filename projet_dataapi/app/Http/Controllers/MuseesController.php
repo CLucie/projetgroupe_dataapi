76c0b56adc32a863e7d8731bcb439808315d5e2e
CreaSoEva <33323626+CreaSoEva@users.noreply.github.com>
2017-12-22 10:15:25 +0100

    selection departements

--- projet_dataapi/app/Http/Controllers/MuseesController.php
+++ projet_dataapi/app/Http/Controllers/MuseesController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 class MuseesController extends Controller
 {
+
+    private $departements;
+
+    public function __construct() {
+        $this->departements = array("Ain","Allier","Ardèche","Cantal","Drôme","Haute-Loire","Haute-Savoie","Isère","Loire","Métropole de Lyon","Puy-de-Dôme","Rhône","Savoie");
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,13 +22,15 @@ class MuseesController extends Controller
     public function index()
     {
         $cartes = Musee::all();
-        return view('accueil' , compact('cartes'));
+        $departements = $this->departements;
+        return view('accueil' , compact('cartes', 'departements'));
     }
 
     public function departement(Request $request)
     {
         $cartes = Musee::departement($request->nom);
-        return view ('accueil' , compact('cartes'));
+        $departements = $this->departements;
+        return view('accueil' , compact('cartes', 'departements'));
     }
 
    
--- projet_dataapi/resources/views/accueil.blade.php
+++ projet_dataapi/resources/views/accueil.blade.php
@@ -1,11 +1,12 @@
 @extends('template')
 @section('main')
+
 <div class="btn-group float-right mr-4">
-<select class="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
-        <option selected>DEPARTEMENTS</option>
-        <option href="{{route('departement',array('nom'=>'Ain'))}}" value="1">AIN</option>
-        <option value="2">ALLIER</option>
-        <option value="3">ARDECHE</option>
+<select onChange="location.href=this.options[this.selectedIndex].value" class="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
+      <option selected>DEPARTEMENTS</option>     
+      @foreach ($departements as $departement)
+       <option value="{{route('departement', array('nom'=>$departement))}}">{{$departement}}</option>
+      @endforeach      
       </select>
 </div>
 
